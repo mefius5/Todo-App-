cfb1ceb0cd3d42b2872ce6117e9eee900a53b024
mefius5 <maciejkowalski1990@o2.pl>
2019-01-08 23:36:41 +0100

    create view and controller

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function new(){
+        echo"some echoed data";
+        return view('new');
+    }
+}
--- resources/views/new.blade.php
+++ resources/views/new.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>New view</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
@@ -64,24 +64,11 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+        
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Our own new view
                 </div>
             </div>
         </div>
