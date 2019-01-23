c9a454dbf1706e0b3d29fe9eefdbc0eecc67f67d
mefius5 <maciejkowalski1990@o2.pl>
2019-01-24 00:26:08 +0100

    add user() and todos moethods in models

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,19 +4,17 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+            <h1>Your todos:</h1>
+            <div class="todo mb-3">
+               
+                @foreach (Auth::user()->todos as $todo)
+                   <div class="card card-body mb-3">
+                       <p class="card-text">{{$todo->id }}</p>
+                       
+                   </div>
+                    @endforeach
             </div>
+            
         </div>
     </div>
 </div>
